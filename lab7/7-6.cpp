2f6545a7c0e02ebfbcdd5bdef4d41bf1ff6e13a7
Nonz007x <comandor969@gmail.com>
2024-02-04 22:09:24 +0700

    fix: show only 3 books and print 3 zeroes when not found

--- lab7/7-6.cpp
+++ lab7/7-6.cpp
@@ -1,32 +1,36 @@
 #include <iostream>
 
-// < coupon
+// < couponValue
 // > 3 books
 // no duplicate
 
-void lexicography(int *arr, int nums[], int n, int c, bool used[], int couponValue) {
-  if (c == 3) {
+void lexicography(int *arr, int nums[], int n, int c, bool used[], int couponValue, bool &found) {
+  if (c == 3 && !found) {
     int sum = 0;
 
     for (int i = 0; i < 3; i++) {
-			sum += nums[arr[i]];
-		}
-
-    if (sum <= couponValue ) {
-      for (int i = 0; i < 3; i++) {
-        std::cout << nums[arr[i]] << " ";
-      }
-      std::cout << "\n";
+      sum += nums[arr[i]];
     }
-  } 
-  else {
-    int sum = 0;
+
+    if (sum <= couponValue) {
+      for (int i = 0; i < 3; i++) {
+        std::cout << nums[arr[i]] << "\n";
+      }
+      found = true;
+    }
+  } else if (!found) {
     for (int i = (c == 0) ? 0 : arr[c - 1] + 1; i < n; i++) {
       arr[c] = i;
-      lexicography(arr, nums, n, c + 1, used, couponValue);
+      lexicography(arr, nums, n, c + 1, used, couponValue, found);
     }
   }
-} 
+
+  if (c == 0 && !found) {
+    for (int i = 0; i < 3; i++) {
+      std::cout << "0\n";
+    }
+  }
+}
 
 int main() {
   int n, couponValue;
@@ -35,9 +39,11 @@ int main() {
   for (int i = 0; i < n; i++) {
     std::cin >> arr[i];
   }
+  std::cout << "\n";
   
   bool used[n] = {false};
-  lexicography(new int[n], arr, n, 0, used, couponValue);
+  bool found = false;
+  lexicography(new int[n], arr, n, 0, used, couponValue, found);
 
   return 0;
 }
